a3d38aa490bc0c57e3a42eb29168a13dcf516451
esatt <e.satterthwaite@outlook.com>
2023-07-10 15:47:08 +0100

    Initial commit

--- app/src/main/java/com/example/arapp/MainActivity.kt
+++ app/src/main/java/com/example/arapp/MainActivity.kt
@@ -1,20 +1,28 @@
 package com.example.arapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.MenuItem
+import android.view.MotionEvent
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.navigation.Navigation
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
 import com.google.ar.core.Config
 import io.github.sceneview.ar.ArSceneView
 import io.github.sceneview.ar.node.ArModelNode
 import io.github.sceneview.ar.node.PlacementMode
 import io.github.sceneview.math.Position
-import io.github.sceneview.math.Scale
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     private lateinit var sceneView: ArSceneView
-    private lateinit var summonAvatarButton: ExtendedFloatingActionButton
-    private lateinit var placeAvatarButton: ExtendedFloatingActionButton
+    private lateinit var avatarButton: ExtendedFloatingActionButton
+    private lateinit var listeningText: TextView
+    private lateinit var micButton: ExtendedFloatingActionButton
 
     data class Model(
         val fileLocation: String,
@@ -23,22 +31,62 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
     )
 
     private var modelNode: ArModelNode? = null
-    var avatarIsLoaded: Boolean = false
+    private var avatarIsLoaded: Boolean = false
+    private var avatarIsPlaced: Boolean = false
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         sceneView = findViewById<ArSceneView?>(R.id.sceneView).apply {
             lightEstimationMode = Config.LightEstimationMode.ENVIRONMENTAL_HDR
         }
-        summonAvatarButton = findViewById<ExtendedFloatingActionButton>(R.id.summonAvatarButton).apply {
-            setOnClickListener {
-                summonAvatar()
+        avatarButton = findViewById<ExtendedFloatingActionButton>(R.id.avatarButton).apply {
+            setOnClickListener { avatarButtonOnClick() }
+        }
+        listeningText = findViewById(R.id.listening_text)
+        micButton = findViewById<ExtendedFloatingActionButton>(R.id.micButton).apply {
+            setOnTouchListener { _, event -> listenToVoice(event)
             }
         }
-        placeAvatarButton = findViewById<ExtendedFloatingActionButton>(R.id.placeAvatarButton).apply {
-            setOnClickListener { placeAvatar() }
+        registerForContextMenu(sceneView)
+    }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        menuInflater.inflate(R.menu.options_menu, menu)
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.option_1 -> return true
+            R.id.option_2 -> return true
         }
+        return super.onContextItemSelected(item)
+    }
+
+    private fun avatarButtonOnClick() {
+        if(!avatarIsLoaded || avatarIsPlaced){
+            modelNode?.let {
+                sceneView.removeChild(it)
+                it.destroy()
+            }
+            loadAvatar()
+            avatarButton.text = getString(R.string.place_avatar_button)
+            avatarIsPlaced = false
+        } else{
+            placeAvatar()
+            avatarButton.text = getString(R.string.summon_avatar_button)
+        }
+
+    }
+
+    private fun placeAvatar() {
+        modelNode?.anchor()
+        avatarIsPlaced = true
     }
 
     private fun loadAvatar(){
@@ -63,17 +111,12 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         avatarIsLoaded = true
     }
 
-    private fun summonAvatar() {
-        if(avatarIsLoaded){
-            modelNode?.let {
-                sceneView.removeChild(it)
-                it.destroy()
-            }
+    private fun listenToVoice(event: MotionEvent): Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> listeningText.isVisible = true
+            MotionEvent.ACTION_UP -> listeningText.isVisible = false
         }
-        loadAvatar()
+        return false
     }
 
-    private fun placeAvatar() {
-        TODO("Not yet implemented")
-    }
 }
---
+++ app/src/main/java/com/example/arapp/TextFragment.kt
@@ -0,0 +1,30 @@
+package com.example.arapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
+
+
+class TextFragment : Fragment() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_text, container, false)
+
+        return view
+    }
+
+
+}
