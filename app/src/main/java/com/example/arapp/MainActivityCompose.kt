34a93b6cff414d824b37c2b013b51360f5629d0b
esatt <e.satterthwaite@outlook.com>
2023-07-13 14:02:18 +0100

    Implemented Sceneview in jetpack compose

--- app/src/main/java/com/example/arapp/MainActivityCompose.kt
+++ app/src/main/java/com/example/arapp/MainActivityCompose.kt
@@ -1,6 +1,10 @@
 package com.example.arapp
 
+import android.annotation.SuppressLint
+import android.graphics.Bitmap.Config
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,37 +14,50 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
 import com.example.arapp.ui.theme.ARAppTheme
+import io.github.sceneview.ar.ArSceneView
+import com.google.ar.core.Config.LightEstimationMode
 
 class MainActivityCompose : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ARAppTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ArView()
                 }
             }
         }
     }
 }
 
+@SuppressLint("InflateParams")
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ArView() {
+    var sceneView: ArSceneView
+
+    fun initArFragment(view: View){
+        sceneView = view.findViewById<ArSceneView>(R.id.sceneView).apply {
+            lightEstimationMode = LightEstimationMode.ENVIRONMENTAL_HDR
+            planeRenderer.isVisible = false
+        }
+    }
+
+    AndroidView(factory = { context ->
+        val view: View = LayoutInflater.from(context).inflate(R.layout.fragment_ar, null)
+        initArFragment(view)
+        view
+    })
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ArViewPreview() {
     ARAppTheme {
-        Greeting("Android")
+        ArView()
     }
 }
