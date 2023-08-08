4285b03f358620aca10d886d8f516dbcb1d700b4
Justin Yeung <la19074@bristol.ac.uk>
2023-08-08 17:14:58 +0100

    Added interfaces for ChatViewModel and DatabaseViewModel.

---
+++ app/src/main/java/com/example/avatar_ai_app/chat/ChatViewModelInterface.kt
@@ -0,0 +1,54 @@
+package com.example.avatar_ai_app.chat
+
+import androidx.lifecycle.LiveData
+import com.example.avatar_ai_app.ui.MainViewModel
+import com.example.avatar_ai_cloud_storage.database.Exhibition
+
+/**
+ * ChatViewModelInterface is the interface between [MainViewModel] and the
+ * Chat ViewModel, which handles all non-UI chat functionality.
+ */
+
+interface ChatViewModelInterface {
+    // ViewModel status types.
+    enum class Status { INIT, READY, RECORDING, PROCESSING }
+
+    // User request types.
+    enum class Request { NAVIGATION, RECOGNITION }
+
+    // ViewModel status.
+    val status: LiveData<Status>
+
+    // Current user request type.
+    val request: LiveData<Request>
+
+    // Requested navigation destination Id.
+    val destinationID: LiveData<String>
+
+    // Informs MainViewModel of error messages.
+    val error: LiveData<MainViewModel.ErrorType>
+
+    // Chat history - newest messages are stored first.
+    val messages: LiveData<MutableList<ChatMessage>>
+
+    // Sets the chat language.
+    fun setLanguage(language: Language)
+
+    // Sets the current exhibition list - an empty list is used by default.
+    fun setExhibitionList(exhibitionList: List<Exhibition>)
+
+    // Provides a new user message to the ChatService.
+    fun newUserMessage(message: String)
+
+    // Starts recording a user message.
+    fun startRecording()
+
+    // Stops recording a user message.
+    fun stopRecording()
+
+    // Describes the given exhibition by chat and audio.
+    fun describeExhibition(exhibitionName: String)
+
+    // Clears the chat history.
+    fun clearChatHistory()
+}
