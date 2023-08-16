e071335840be64bf652528d160c45f639e00f51b
Justin Yeung <la19074@bristol.ac.uk>
2023-08-17 00:52:39 +0100

    Bugfix: modified the fuzzy searching algorithm to apply the minimum Levenshtein ratio to individual word matches.

--- app/src/main/java/com/example/avatar_ai_app/shared/StringExtensions.kt
+++ app/src/main/java/com/example/avatar_ai_app/shared/StringExtensions.kt
@@ -1,13 +1,13 @@
 package com.example.avatar_ai_app.shared
 
-import android.util.Log
 import com.example.avatar_ai_app.shared.StringExtensionsConstants.levenshteinDistance
+import com.example.avatar_ai_app.shared.StringExtensionsConstants.phraseParserPattern
 import com.example.avatar_ai_app.shared.StringExtensionsConstants.wordSeparatorRegex
 import org.apache.commons.text.similarity.LevenshteinDistance
 
-private const val TAG = "StringExtensions"
-
 private object StringExtensionsConstants {
+    const val phraseParserPattern = "(^|\\s)%s(\\s|\$)"
+
     // LevenshteinDistance instance for fuzzy comparison.
     val levenshteinDistance = LevenshteinDistance()
 
@@ -24,61 +24,45 @@ private object StringExtensionsConstants {
  * @return `true` if the string contains the phrase, `false` otherwise.
  */
 fun String.containsPhrase(phrase: String): Boolean {
-    val regex = Regex("(^|\\s)$phrase(\\s|\$)", RegexOption.IGNORE_CASE)
+    val regex = Regex(phraseParserPattern.format(phrase), RegexOption.IGNORE_CASE)
     return this.contains(regex)
 }
 
 /**
- * Checks if a given phrase is approximately matched within the string.
+ * Checks if the current string contains the given phrase with a fuzzy matching approach.
  *
- * Compares its similarity with combinations of words from the string using the Levenshtein ratio.
- * The combinations contain the same number of words as the phrase.
+ * Applies the provided minimum Levenshtein distance ratio to individual word matches.
  *
  * @param phrase The phrase to search for.
- * @param minLevenshteinRatio The minimum Levenshtein similarity ratio for a match to occur.
- * @return `true` if a similar phrase is found, `false` otherwise.
+ * @param minLevenshteinRatio The minimum acceptable Levenshtein distance ratio for a word match.
+ * @return True if a fuzzy match is found,¬ false otherwise.
  */
 fun String.containsPhraseFuzzy(phrase: String, minLevenshteinRatio: Double): Boolean {
-    val phraseLowerCase = phrase.lowercase()
-    // Count the number of words in the target phrase.
-    val wordsInPhrase =
-        phraseLowerCase.splitToSequence(wordSeparatorRegex).count()
+    val messageWords = this.split(wordSeparatorRegex)
+    val phraseWords = phrase.split(wordSeparatorRegex)
+    val phraseWordsSize = phraseWords.size
+    var phraseWordIndex = 0
 
-    this.lowercase().generateCombinations(wordsInPhrase).forEach {
-        val levenshteinRatio = calculateLevenshteinRatio(it, phraseLowerCase)
-
-        if (levenshteinRatio >= minLevenshteinRatio) {
-            Log.i(TAG, "containsPhraseFuzzy: $it - $phraseLowerCase ($levenshteinRatio)")
+    messageWords.forEach {
+        if (phraseWordIndex >= phraseWordsSize) {
             return true
+        } else if (it.calculateLevenshteinRatio(phraseWords[phraseWordIndex]) >= minLevenshteinRatio) {
+            phraseWordIndex++
+        } else if (phraseWordIndex != 0) {
+            return false
         }
     }
-    return false
+    return phraseWordIndex == phraseWordsSize
 }
 
-/*
-* Generates combinations of words from the string based on the provided
-* combination size. For example, if combination size is three, it
-* generates sets of three consecutive words.
+/**
+ * Calculates the Levenshtein distance ratio between the current string and a target string.
+ *
+ * @param string The target string for which the Levenshtein distance ratio is calculated.
+ * @return The Levenshtein distance ratio between the two strings.
  */
-private fun String.generateCombinations(combinationSize: Int): List<String> {
-    val wordList = this.split(wordSeparatorRegex)
-    val combinationsList = mutableListOf<String>()
-
-    for (i in 0..wordList.size - combinationSize) {
-        combinationsList.add(
-            wordList.subList(i, i + combinationSize).joinToString(" ")
-        )
-    }
-    return combinationsList
-}
-
-/*
-* Calculates the Levenshtein similarity ratio between two strings.
-* The Levenshtein distance is normalized by the maximum length of the strings.
- */
-private fun calculateLevenshteinRatio(string1: String, string2: String): Double {
-    val distance = levenshteinDistance.apply(string1, string2)
-    val maxLength = maxOf(string1.length, string2.length)
-
-    return 1.0 - distance.toDouble() / maxLength
+fun String.calculateLevenshteinRatio(string: String): Double {
+    val distance = levenshteinDistance.apply(this.lowercase(), string.lowercase())
+    val maxLength = maxOf(this.length, string.length)
+    return 1 - distance.toDouble() / maxLength
 }
